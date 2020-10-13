f1fdd9a252fd0423b80e3fd051a2cd7b98a32c9d
jeeHwon <jeehwon01@gmail.com>
2020-10-13 15:22:19 +0900

    email update

--- javaSolvedEx/src/ch02/ex1330.java
+++ javaSolvedEx/src/ch02/ex1330.java
@@ -1,11 +1,11 @@
 package ch02;
 import java.util.Scanner;
 public class ex1330 {
+	//ฟฌวั
 	public static void main(String[] args) {
 		Scanner num = new Scanner(System.in);
 		int a = num.nextInt();
 		int b = num.nextInt();
-		
 		if (a<b) {
 			System.out.println("<");
 			
