680431c3a28599dedbb6baf2b139b6288d0d052d
jeeHwon <jeehwon01@gmail.com>
2020-10-16 15:51:55 +0900

    ex8958

--- javaSolvedEx/src/ch05/ex8958.java
+++ javaSolvedEx/src/ch05/ex8958.java
@@ -1,5 +1,30 @@
 package ch05;
 
-public class ex8958 {
+import java.util.Scanner;
 
+public class ex8958 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		String[] str = new String[n];
+		//String fake = sc.nextLine();
+		for (int i = 0; i < n; i++) {
+			str[i] = sc.next();
+		}
+		for (int i = 0; i < n; i++) {
+			char[] arr = str[i].toCharArray();
+			int tot = 0;
+			int count = 0;
+			for (int j = 0; j < arr.length; j++) {
+				if (arr[j]=='O') {
+					tot = tot + (count+1);
+					count = count + 1;
+				} else {
+					count = 0;
+				}
+			}
+			System.out.println(tot);
+		}
+		sc.close();
+	}
 }
