0de1ee71dd800c2818856f6c1e09d65fd3a21cce
jeeHwon <jeehwon01@gmail.com>
2020-10-21 11:02:10 +0900

    ch06 fin

--- javaSolvedEx/src/ch06/ex1065.java
+++ javaSolvedEx/src/ch06/ex1065.java
@@ -5,19 +5,13 @@ import java.util.Scanner;
 
 public class ex1065 {
 	public static void main(String[] args) {
-		//468 531 751
-		//110
-		// 1 2 3 4 5 6 7 8 9 = 9
-		// 1 ~ 99 = 99
-		//100 101 102 103 104 105 106 107 108 109 110
-		//789 757
+
 		Scanner sc = new Scanner(System.in);
 		ArrayList arr = new ArrayList();
 		for (int i = 1; i < 100; i++) {
 			arr.add(i);
 		}
 		int n = sc.nextInt();
-		
 		if (n<100) {
 			System.out.println(n);
 		}else {
@@ -28,14 +22,8 @@ public class ex1065 {
 				if (b-a==c-b) {
 					arr.add(i);
 				}
-				
 			}
 			System.out.println(arr.size());
-			
 		}
-		
-		
-		
 	}
-	
 }
