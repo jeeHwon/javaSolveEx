d5ebc9fc1e9a7f583149fac29937df8a73a17ef2
jeeHwon <jeehwon01@gmail.com>
2020-11-05 18:08:37 +0900

    ex2775_fin

--- javaSolvedEx/src/ch08/ex2775.java
+++ javaSolvedEx/src/ch08/ex2775.java
@@ -4,21 +4,20 @@ import java.util.Scanner;
 
 public class ex2775 {
 	public static void main(String[] args) {
-		//testCase : 케이스 수
-		// k : 몇 층 2 
-		// n : 몇 호 3
 		Scanner sc = new Scanner(System.in);
 		int testCase = sc.nextInt();
-		
 		
 		for (int i = 0; i < testCase; i++) {
 			int k = sc.nextInt();
 			int n = sc.nextInt();
-			int[][] arr = new int[n][k];
-			for (int j = 0; j < k; j++) {
+			//아파트 모양의 배열 생성
+			int[][] arr = new int[k+1][n+1];
+			//0층에 먼저 기본값 세팅해준다
+			for (int j = 0; j < n; j++) {
 				arr[0][j] = j+1;
 			}
-			for (int j = 1; j < k; j++) {
+			//1층씩 올라가면서 각 배열에 이전 배열의 합을 더해 넣는다
+			for (int j = 1; j < k+1; j++) {
 				for (int j2 = 0; j2 < n; j2++) {
 					int hap = 0;
 					for (int l = 0; l < j2+1; l++) {
@@ -27,9 +26,7 @@ public class ex2775 {
 					arr[j][j2] = hap;
 				}
 			}
-			System.out.println(arr[n-1][k-1]);
+			System.out.println(arr[k][n-1]);
 		}
-		
-		
 	}
 }
